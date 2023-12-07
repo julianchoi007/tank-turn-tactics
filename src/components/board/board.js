c0a3557e75cb3c6ae1d3a3b37066973f2e329299
julianchoi007 <choijulian007@gmail.com>
2023-12-06 22:48:33 -0500

    added tanks

--- src/components/board/board.js
+++ src/components/board/board.js
@@ -1,21 +1,42 @@
+import { useEffect, useRef, useState } from "react";
 import { Tile } from "../tile/tile";
 import "./board.css";
 
-const boardSize = 16;
+const boardSize = 9;
 export const Board = () => {
-  let tiles = Array(boardSize).fill(Array(boardSize).fill(0));
+  const [tiles, setTiles] = useState(
+    [...Array(boardSize)].map((r) => [...Array(boardSize)].map((r) => "tile"))
+  );
+  const [message, setMessage] = useState("test");
+  useEffect(() => {
+    let arr = tiles;
+    for (let i = 1; i < boardSize; i += 3) {
+      for (let j = 1; j < boardSize; j += 3) {
+        arr[i][j] = "tank";
+      }
+    }
+    console.log("test");
+    setTiles(arr);
+  }, []);
   console.log(tiles);
   return (
     <div className="board-wrapper">
       <div className="board">
-        {tiles.map((element) => (
+        {tiles.map((rows) => (
           <div className="row">
-            {tiles.map((element) => (
-              <Tile />
+            {rows.map((square) => (
+              <Tile type={square} />
             ))}
           </div>
         ))}
       </div>
+      <button
+        onClick={() => {
+          setMessage("tests");
+        }}
+      >
+        test
+      </button>
     </div>
   );
 };
--- src/components/tile/tile.js
+++ src/components/tile/tile.js
@@ -1,5 +1,17 @@
 import "./tile.css";
 
-export const Tile = () => {
-  return <div className="tile"></div>;
+export const Tile = (props) => {
+  if (props.type == "tile") {
+    return <div className="tile"></div>;
+  } else if (props.type == "tank") {
+    return (
+      <div className="tile">
+        <img
+          src="tank.png"
+          alt="tank"
+          style={{ width: "80px", height: "80px", color: "red" }}
+        />
+      </div>
+    );
+  }
 };
